ab229ab8997b7dd2d6e247b18329d7e829599480
Kampela <kampela@kampela-pc.arcanys.com>
2016-02-05 17:06:40 +0800

    Added test for GET /posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
     it "should display some posts" do
-    	visit posts_path
-    	page.should have_content 'sample post'
+      @post = Post.create title: 'Test Title', content: 'Some Post'
+
+      visit posts_path
+      page.should have_content 'Test Title'
+      
    	end
   end
 end
