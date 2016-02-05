c665159f8585ccfdf954e23ed21b009c3c439e6e
Kampela <kampela@kampela-pc.arcanys.com>
2016-02-05 18:07:23 +0800

    Added update action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
+
+  before do
+  	@post = Post.create title: 'Test Title', content: 'Some Post'
+  end
+
   describe "GET /posts" do
     it "should display some posts" do
-      @post = Post.create title: 'Test Title', content: 'Some Post'
-
       visit posts_path
       page.should have_content 'Test Title'
-
    	end
 
    	it "can create a new post" do
@@ -25,4 +27,27 @@ RSpec.describe "Posts", type: :request do
    	end
 
   end
+
+  describe "PUT /posts" do
+  	it "edits a post" do
+  	  visit posts_path
+  	  click_link 'Edit'
+
+  	  current_path.should == edit_post_path(@post)
+  	  find_field('post_title').value.should 'Test Title'
+  	  find_field('post_content').value.should 'Some Post'
+
+      fill_in 'post_title', :with => 'updated title'
+      fill_in 'post_content', :with => 'updated content'
+      check('post_is_private')
+      click_button 'Update Post'
+      
+      current_path.should == posts_path
+      page.should have_content 'updated title'
+  	  page.should have_content 'updated content'
+
+  	end
+
+  end
+
 end
