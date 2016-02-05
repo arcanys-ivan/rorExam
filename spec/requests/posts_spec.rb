2f0744fac46a43e098f636abd8745630c0ea127e
Kampela <kampela@kampela-pc.arcanys.com>
2016-02-05 17:41:05 +0800

    Added test for creating new post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -7,7 +7,22 @@ RSpec.describe "Posts", type: :request do
 
       visit posts_path
       page.should have_content 'Test Title'
-      
+
    	end
+
+   	it "can create a new post" do
+   	  visit posts_path
+   	  within("#new_post") do
+        fill_in 'post_title', :with => 'a title'
+        fill_in 'post_content', :with => 'some message'
+        uncheck('post_is_private')
+      end
+
+      click_button 'Create Post'
+
+      current_path.should == posts_path
+      page.should have_content 'a title'
+   	end
+
   end
 end
