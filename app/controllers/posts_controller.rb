34810bc203eaa2be49836c58a71bf3a3ca0ccb6e
Kampela <kampela@kampela-pc.arcanys.com>
2016-02-05 17:39:59 +0800

    Added action create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,21 @@ class PostsController < ApplicationController
   def index
   	@posts = Post.all
   end
-  
+
+  def create
+  	# @post = Post.new(params[:post])    
+   #  @post.save
+    Post.create post_params
+    redirect_to :back
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :is_private)
+  end
+
+  # def post_params
+  #   params.require(:post).permit(:title, :content, :is_private)
+  # end
 end
